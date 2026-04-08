4467e59edbeadca38b7e8d1dfda2f538959d29d7
waxir <danishdarman06@gmail.com>
2026-04-09 02:07:55 +0500

    useCallback dependiencies

--- password-gen/src/App.jsx
+++ password-gen/src/App.jsx
@@ -1,17 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { useCallback, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8);
+  const [nmbrAllowed, setNmbrAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
 
+  const passwordGenerator = useCallback(() => {
+    
+  }, [
+    length,
+    nmbrAllowed,
+    charAllowed,
+    setPassword,
+  ]);
   return (
     <>
-    <h1 className='text-4xl text-center text-white'>Password Generator</h1>
-
-
-      
+      <h1 className="text-4xl text-center text-white">Password Generator</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
