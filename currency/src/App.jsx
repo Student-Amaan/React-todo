3bb0ba621e12db53c791f3491821b09bc94e2291
waxir <danishdarman06@gmail.com>
2026-04-11 02:35:52 +0500

    input field with import

--- currency/src/App.jsx
+++ currency/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 import './App.css'
 import InputBox from './Components/Inputbox'
+import Customhook from './Hooks/Customhook'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <InputBox />
+      
     </>
   )
 }
