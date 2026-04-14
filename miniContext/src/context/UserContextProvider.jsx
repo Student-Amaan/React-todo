e379612c5a6f49821b3c64a5b0587e134c96b712
waxir <danishdarman06@gmail.com>
2026-04-14 18:38:50 +0500

    userContextProvider commited

--- miniContext/src/context/UserContextProvider.jsx
+++ miniContext/src/context/UserContextProvider.jsx
@@ -1,1 +1,15 @@
 import React from "react";
+
+import userContext from "./UserContext";
+
+const userContextProvider = ({children}) => {
+    const [user , setUser] = React.useState(null)
+
+    return (
+        <userContext.Provider value={{user , setUser}}>
+        {children}
+        </userContext.Provider>
+    )
+}
+
+export default userContextProvider
