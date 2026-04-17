1113793e5edd768bae005ef88ca3717e50ea591d
waxir <danishdarman06@gmail.com>
2026-04-17 19:13:04 +0500

    file change commited

--- miniContext/src/App.jsx
+++ miniContext/src/App.jsx
@@ -4,23 +4,15 @@ import userContext from './context/UserContext'
 import './App.css'
 
 function App() {
-  const [userName, setUserName] = useState("")
-  const [password , setPassword] = useState("")
+  
 
-
-  const handleSubmit = () => {
-    
-  }
+ 
 
   return (
     <>
-     <div>
-      <h2>Aman aur react</h2>
-      <input type="text" placeholder='enter value...' />
-      <button onClick={handleSubmit}>Submit</button>
-     </div>
+      
     </>
-  )
+  );
 }
 
 export default App
--- miniContext/src/Components/Login.jsx
+++ miniContext/src/Components/Login.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
 function Login() {
+
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { setUser } = useContext(userContext);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUser({ userName, password });
+  };
+
+
   return (
-    <div>Login</div>
-  )
+    <div>
+      <h2>Aman aur react</h2>
+      <input
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+        type="text"
+        placeholder="username"
+      />
+      <button
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        onClick={handleSubmit}
+      >
+        Submit
+      </button>
+    </div>
+  );
 }
 
 export default Login
